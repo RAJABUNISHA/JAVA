d156533fbe255f3840d6e337b17098518ee0f6c0
RAJABUNISHA <36623634+RAJABUNISHA@users.noreply.github.com>
2018-05-16 15:49:24 +0530

    Create armstrong.java

---
+++ armstrong.java
@@ -0,0 +1,33 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		try{
+			int n = Integer.parseInt(s);
+			int temp=n;
+			int c=0;
+			while(n!=0){
+				int a = n%10;
+				n=n/10;
+				c=c+(a*a*a);
+				
+			}
+			if(temp==c)
+			System.out.println("yes it is amstrong");
+			else
+			System.out.println("no it is no amstrong");
+			
+		}
+	         catch(NumberFormatException nef){
+			System.out.println("invalid input");
+		}
+	}
+}
