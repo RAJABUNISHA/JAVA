6a5e980d671d8b2cd970a36829711ab467a93a0e
RAJABUNISHA <36623634+RAJABUNISHA@users.noreply.github.com>
2018-05-16 15:07:49 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	     Scanner sc = new Scanner(System.in);
+	     String s1 = sc.nextLine();
+	     String s2 = sc.nextLine();
+	     try{
+	     	int n = Integer.parseInt(s1);
+	            int q = Integer.parseInt(s2);
+	     	for(int i=n;i<q;i++){
+	     	 if(i%2!=0)
+	     	    System.out.println(+i);
+	     	}
+	     	}
+	     catch(NumberFormatException nef){
+	     	System.out.println("invalid input");
+	     }
+	}
+}
